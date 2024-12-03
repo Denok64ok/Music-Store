8ed1f03a384389ee47c103c0f5e0de0099379cbf
denok64ok <yaden9483@gmail.com>
2024-12-03 13:07:14 +0500

    TailwindCSS

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 import router from './router';
+import './main.css';
 
 const app = createApp(App);
 app.use(createPinia());
-app.mount('#app');
 app.use(router);
+app.mount('#app');
