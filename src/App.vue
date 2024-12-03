dae21b297b870d50cd2ad2b0afeab558b4901bb0
denok64ok <yaden9483@gmail.com>
2024-12-03 13:42:38 +0500

    Work TailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
       <router-link to="/">Главная</router-link>
       <router-link to="/cart">Корзина</router-link>
     </nav>
-
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 import router from './router';
-import './main.css';
+import './index.css';
 
 const app = createApp(App);
 app.use(createPinia());
